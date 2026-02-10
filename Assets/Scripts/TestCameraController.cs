bc266ba8cc70b46631ec930d1b66aa82106d8ea8
Daria Gronska <gronskaadariaa@gmail.com>
2026-02-10 23:16:16 +0100

    add player model

--- Assets/Scripts/TestCameraController.cs
+++ Assets/Scripts/TestCameraController.cs
@@ -6,7 +6,7 @@ public class TestCameraController : MonoBehaviour
     private Transform player;
     
     private float yOffset=2f;
-    private float zOffset=-5f;
+    private float zOffset=-3.5f;
    
     void Start()
     {
